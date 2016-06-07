db41d0cd740e9e525c3ac4e1205e4ce27e465974
David Wilcock <dwilcock@doc-net.com>
2016-06-07 16:07:11 +0100

    Exception Factory tweaks (#27)
    * Change: Renamed Exception\Handler to Exception\Factory - updated usage
    Change: Renamed deliver() method to build() - updated usage
    
    * Change throws for returns
    
    * Changed to how we throw exception
    
    Now throwing Factory::build() explicitly in prepare()

--- src/Docnet/DB/Exception/Factory.php
+++ src/Docnet/DB/Exception/Factory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Handler.php
+ * Factory.php
  *
  * @author David Wilcock (dwilcock@doc-net.com)
  * @copyright Venditan Limited 2016
@@ -8,7 +8,7 @@
 
 namespace Docnet\DB\Exception;
 
-class Handler
+class Factory
 {
 
    const CODE_MYSQL_GONE_AWAY = 2006;
@@ -19,15 +19,15 @@ class Handler
     * @throws DatabaseConnectionException
     * @throws \Exception
     */
-   public static function deliver($strMessage, $intCode = null) {
+   public static function build($strMessage, $intCode = null) {
 
       switch ($intCode) {
 
          case self::CODE_MYSQL_GONE_AWAY:
-            throw new DatabaseConnectionException($strMessage, $intCode);
+            return new DatabaseConnectionException($strMessage, $intCode);
 
          default:
-            throw new \Exception($strMessage, $intCode);
+            return new \Exception($strMessage, $intCode);
 
       }
 
